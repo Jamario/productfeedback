25de234cf136b2075dc35514dd12e9e5ad7b1403
Jamario Thorne <jamariothorne@gmail.com>
2023-03-23 08:02:31 -0400

    Font setup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Jost } from "next/font/google";
+
+const jost = Jost({ variable: "--font-jost", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={`${jost.variable} font-sans`}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
 
-        // Or if using `src` directory:
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "theme-purple": "#AD1FEA",
+                "theme-blue": {
+                    DEFAULT: "#4661E6",
+                    light: "#62BCFA",
+                    dark: "#3A4374",
+                    darkest: "#4661E6",
+                },
+                "theme-grey": {
+                    DEFAULT: "#F2F4FF",
+                    light: "#F7F8FD",
+                    dark: "#647196",
+                },
+                "theme-orange": "#F49F85",
+            },
+            fontFamily: {
+                sans: ["var(--font-jost)", ...fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 };
