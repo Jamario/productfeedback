cfcf8f5c583d74a18bab6bb0c5cceb76598be89b
Jamario Thorne <jamariothorne@gmail.com>
2023-04-21 19:05:01 -0400

    Finished right nav menu for mobile view.

--- components/Indicator.tsx
+++ components/Indicator.tsx
@@ -1,18 +1,20 @@
 interface IndicatorProps {
     isActive: boolean;
     children: React.ReactNode;
+    onClick: () => void;
     className?: string;
 }
 
-const Indicator = ({ isActive, children, className }: IndicatorProps) => {
+const Indicator = ({ isActive, children, className, onClick }: IndicatorProps) => {
     return (
-        <div
+        <button
             className={`py-2 px-4 rounded-xl inline-block font-bold ${
                 isActive ? "bg-theme-blue text-theme-grey" : "bg-theme-grey text-theme-blue hover:bg-[#CFD7FF]"
             } ${className}`}
+            onClick={onClick}
         >
             {children}
-        </div>
+        </button>
     );
 };
 
