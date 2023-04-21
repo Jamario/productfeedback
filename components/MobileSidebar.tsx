b18e5e47cf3dfdd9fd44555eeeb5619c3d0e9d9a
Jamario Thorne <jamariothorne@gmail.com>
2023-04-21 19:19:15 -0400

    Moved all of the sidebar content to the sidebar component because it is not reusable; there is one sidebar.

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import Indicator from "./Indicator";
+
+interface MobileSidebarProps {
+    isSideBarOpen: boolean;
+    filters: string[];
+    activeFilter: string;
+    setActiveFilter: (activeFilter: string) => void;
+    className?: string;
+}
+
+const MobileSidebar = ({ isSideBarOpen, className, filters, activeFilter, setActiveFilter }: MobileSidebarProps) => {
+    const getFilterMap = () =>
+        filters.map((filter) => {
+            return (
+                <Indicator
+                    key={filter}
+                    isActive={activeFilter === filter}
+                    onClick={() => setActiveFilter(filter)}
+                    className="text-sm"
+                >
+                    {filter}
+                </Indicator>
+            );
+        });
+
+    return (
+        <>
+            <div
+                className={`absolute top-0 bottom-0 right-0 left-0 bg-black bg-opacity-50 ${
+                    isSideBarOpen ? "absolute" : "hidden"
+                }`}
+            />
+            <div
+                className={`w-[270px] bg-theme-grey-light absolute top-0 right-0 bottom-0 transition-transform duration-300 ease-in-out p-6 ${
+                    isSideBarOpen ? "translate-x-0" : "translate-x-full"
+                } ${className}`}
+            >
+                <div className="rounded-lg w-full bg-white mb-6 pl-5 pt-5 pb-7 pr-4 min-h-[180px] flex flex-row content-between justify-between flex-wrap">
+                    {getFilterMap()}
+                </div>
+                <div className="rounded-lg w-full bg-white p-6">
+                    <div className="flex justify-between mb-6 items-center">
+                        <strong className="text-lg text-theme-blue-dark">Roadmap</strong>
+                        <Link href="/roadmap" className="text-blue-500 hover:text-blue-700 underline">
+                            View
+                        </Link>
+                    </div>
+                    <div className="flex items-center text-theme-grey-dark">
+                        <div className="h-2 w-2 rounded-full bg-theme-orange mr-4" />
+                        <div className="flex-1">Planned</div>
+                        <strong>2</strong>
+                    </div>
+                    <div className="flex items-center text-theme-grey-dark">
+                        <div className="h-2 w-2 rounded-full bg-theme-purple mr-4" />
+                        <div className="flex-1">In-Progress</div>
+                        <strong>3</strong>
+                    </div>
+                    <div className="flex items-center text-theme-grey-dark">
+                        <div className="h-2 w-2 rounded-full bg-theme-blue-light mr-4" />
+                        <div className="flex-1">Live</div>
+                        <strong>1</strong>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default MobileSidebar;
--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import Link from "next/link";
+
 import Header from "../headers/MainHeader";
-import Sidebar from "../Sidebar";
-import Indicator from "../Indicator";
+import MobileSidebar from "../MobileSidebar";
 
 type MainLayoutProps = {
     children: React.ReactNode;
@@ -14,54 +13,17 @@ const MainLayout = ({ children }: MainLayoutProps) => {
     const [isSideBarOpen, setIsSideBarOpen] = useState(true);
     const [activeFilter, setActiveFilter] = useState("All");
 
-    const getFilterMap = () =>
-        filters.map((filter) => {
-            return (
-                <Indicator
-                    key={filter}
-                    isActive={activeFilter === filter}
-                    onClick={() => setActiveFilter(filter)}
-                    className="text-sm"
-                >
-                    {filter}
-                </Indicator>
-            );
-        });
-
-    console.log("active filter is", activeFilter);
     return (
         <div className="flex flex-col h-screen">
             <Header isSideBarOpen={isSideBarOpen} setIsSideBarOpen={setIsSideBarOpen} />
             <div className="relative flex-1 overflow-x-hidden">
                 <main className="flex-1">{children}</main>
-                <Sidebar isSideBarOpen={isSideBarOpen} className="p-6">
-                    <div className="rounded-lg w-full bg-white mb-6 pl-5 pt-5 pb-7 pr-4 min-h-[180px] flex flex-row content-between justify-between flex-wrap">
-                        {getFilterMap()}
-                    </div>
-                    <div className="rounded-lg w-full bg-white p-6">
-                        <div className="flex justify-between mb-6 items-center">
-                            <strong className="text-lg text-theme-blue-dark">Roadmap</strong>
-                            <Link href="/roadmap" className="text-blue-500 hover:text-blue-700 underline">
-                                View
-                            </Link>
-                        </div>
-                        <div className="flex items-center text-theme-grey-dark">
-                            <div className="h-2 w-2 rounded-full bg-theme-orange mr-4" />
-                            <div className="flex-1">Planned</div>
-                            <strong>2</strong>
-                        </div>
-                        <div className="flex items-center text-theme-grey-dark">
-                            <div className="h-2 w-2 rounded-full bg-theme-purple mr-4" />
-                            <div className="flex-1">In-Progress</div>
-                            <strong>3</strong>
-                        </div>
-                        <div className="flex items-center text-theme-grey-dark">
-                            <div className="h-2 w-2 rounded-full bg-theme-blue-light mr-4" />
-                            <div className="flex-1">Live</div>
-                            <strong>1</strong>
-                        </div>
-                    </div>
-                </Sidebar>
+                <MobileSidebar
+                    isSideBarOpen={isSideBarOpen}
+                    filters={filters}
+                    activeFilter={activeFilter}
+                    setActiveFilter={setActiveFilter}
+                />
             </div>
         </div>
     );
